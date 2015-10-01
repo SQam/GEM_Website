9b2ee8edfb7e80a8fe0861218678053aac52850c
liambluebox <wak135375@yahoo.com>
2015-10-01 20:41:44 +0000

    Updated some packages for Windows portability.
    This should allow development from Windows.

--- app/packages/gemapp-lib/package.js
+++ app/packages/gemapp-lib/package.js
@@ -1,16 +1,26 @@
 Package.describe({
   name: 'gemapp:lib',
   summary: 'GemApp third party libraries and global namespace.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: ''
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(['METEOR@1.0']);
+  api.versionsFrom(['METEOR@1.2']);
 
   var packages = [
     'standard-app-packages',
-    'service-configuration',
+    //'service-configuration',
+    'meteor-base',
+    'mobile-experience',
+    'mongo',
+    'blaze-html-templates',
+    'session',
+    'jquery',
+    'tracker',
+    'standard-minifiers',
+    'es5-shim',
+    'ecmascript',
     'accounts-ui',
     'accounts-base',
     'accounts-password',
@@ -25,14 +35,18 @@ Package.onUse(function (api) {
     'coffeescript',
     'email',
     'iron:router',
-    'jquery',
-    'meteor-platform',
     'mrt:moment',
-    'particle4dev:sass@0.3.0',
+    'fourseven:scss@2.1.1',
     'pinglamb:bootstrap3',
     'random',
     'themeteorchef:jquery-validation',
     'underscore',
+    //'meteorhacks:kadira',
+    'meteorhacks:fast-render',
+    'fastclick',
+    //'msavin:mongol',
+    //'aldeed:autoform',
+    //'particle4dev:upload-avatar',
   ];
 
   api.use(packages);
