5bfb305cd307b10c3bb29b709b14a4caf0066f56
unknown <joyziar@gmail.com>
2015-10-26 17:12:07 -0700

    Test on Allowing client to insert items to db

--- app/collections.js
+++ app/collections.js
@@ -1,15 +1,31 @@
+
+	EmailList = new Meteor.Collection('subEmails');
+
+
 if (Meteor.isClient){
-	console.log("INPUT???");
+console.log("INPUT????");
+
+Template.emailSub.events({
+	'click .emailSubmit': function(){
+		EmailList.insert({
+			subEmail: $('.the_email').val()
+		});
+		$('.the_email').val('');
+		console.log($('.the_email').val());
+	}
 	
+});
+
+
 }
 
 if (Meteor.isServer){
+	
+EmailList.allow({
+  insert: function (userId, doc) {
+    return true;
+  }
+})
+	console.log("nope");
 
-EmailList = new Meteor.Collection('subEmail');
-EmailList.insert({
-			subEmails: "jo@gmail.com"
-		});
-		
-		
-console.log("Made???")
 }
